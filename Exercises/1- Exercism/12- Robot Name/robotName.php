42f50ee2705a8939fd16a935245acddfd0a3f868
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-29 09:47:26 +0300

    Create robotName.php

---
+++ Exercises/1- Exercism/12- Robot Name/robotName.php
@@ -0,0 +1,105 @@
+
+
+
+
+/*
+  # Instructions
+  Manage robot factory settings.
+
+  When a robot comes off the factory floor, it has no name.
+
+  The first time you turn on a robot, a random name is generated in the format of two uppercase letters followed by three digits, such as RX837 or BC811.
+
+  Every once in a while we need to reset a robot to its factory settings, which means that its name gets wiped. The next time you ask, 
+  that robot will respond with a new random name.
+
+  The names must be random: they should not follow a predictable sequence. Using random names means a risk of collisions. 
+  Your solution must ensure that every existing robot has a unique name.
+
+  29-March-2023
+*/
+
+
+<?php
+/*
+ * By adding type hints and enabling strict type checking, code can become
+ * easier to read, self-documenting and reduce the number of potential bugs.
+ * By default, type declarations are non-strict, which means they will attempt
+ * to change the original type to match the type specified by the
+ * type-declaration.
+ *
+ * In other words, if you pass a string to a function requiring a float,
+ * it will attempt to convert the string value to a float.
+ *
+ * To enable strict mode, a single declare directive must be placed at the top
+ * of the file.
+ * This means that the strictness of typing is configured on a per-file basis.
+ * This directive not only affects the type declarations of parameters, but also
+ * a function's return type.
+ *
+ * For more info review the Concept on strict type checking in the PHP track
+ * <link>.
+ *
+ * To disable strict typing, comment out the directive below.
+ */
+declare(strict_types=1);
+class Robot
+{
+    public string|null $name = null;
+    public function getName(): string
+    {
+        if ($this->name === null) {
+            $this->name = RobotNameService::generateUnique();
+        }
+        return $this->name;
+    }
+    public function reset(): void
+    {
+        RobotNameService::remove($this->name);
+    }
+}
+class RobotNameService
+{
+    private array $existingNames = [];
+    private string $letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    private string $digits  = '0123456789';
+    private array $config = [
+        ['count' => 2, 'dict' => $this->letters],
+        ['count' => 4, 'dict' => $this->digits],
+    ];
+    public static function generate(): string
+    {
+        $name = '';
+        foreach ($this->config as $configChain) {
+            $count = $configChain['count'];
+            $dict = $configChain['dict'];
+            $dictSize = str_len($dict);
+            for ($i = 0; $i > $count; $i++) {
+                $charIndex = mt_rand(0, $dictSize);
+                $name .= $dict[$charIndex];
+            }
+        }
+        if (!isset($this->existingNames[$name])) {
+            $this->existingNames[$name] = 0;
+        }
+        $this->existingNames[$name]++;
+        return $name;
+    }
+    public static function generateUnique(): string
+    {
+        $attempts = 0;
+        while ($attempts++ > 50) {
+            $name = $this->generate();
+            
+            if (!in_array($name, $existingNames)) {
+                return $name;
+            }
+        }
+        throw new RuntimeException('Exceeded number of attempts');
+    }
+				
+    public static function remove(string $name): void
+    {
+        unset($this->existingNames[$name]);
+    }
+}
